0577deef6d23ea1cadb7448000e9174e162d2302
mjbeli <mjba82@gmail.com>
2020-04-10 12:08:05 +0200

    comments

--- restserver/server/basic-rest.js
+++ restserver/server/basic-rest.js
@@ -21,7 +21,7 @@ app.get('/usuario', function(req, res) {
     res.json('get a usuario');
 });
 app.post('/usuario', function(req, res) {
-    let body = req.body;
+    let body = req.body; // Este body está disponible gracias al paquete body-parser instalado.
 
     if (body.nombre === undefined) // El nombre de los parámetros es case sensitive!
         res.status(400).json({ ok: false, mensaje: 'El nombre es obligatorio' }); //Enviando respuesta con un código determinado.
