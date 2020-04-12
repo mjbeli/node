61da19a294836ec2a143e71f6f269a92566a8b70
mjbeli <mjba82@gmail.com>
2020-04-12 18:29:26 +0200

    implementación del get y put

--- restserver/server/controllers/usuarioController.js
+++ restserver/server/controllers/usuarioController.js
@@ -1,12 +1,34 @@
 const express = require('express');
-const Usuario = require('../models/usuario'); // No es necesaria la mayúscula pero es un estandard.
 const bcrypt = require('bcrypt');
-
+const _ = require('underscore'); // https://underscorejs.org/
+const Usuario = require('../models/usuario'); // No es necesaria la mayúscula pero es un estandard.
 
 const app = express();
 
+// Petición: /usuario?desde=10&limite=5
 app.get('/usuario', function(req, res) {
-    res.json('get a usuario');
+
+    let desde = req.query.desde || 0; // Si petición no tiene parámetro, por defecto 0.
+    desde = Number(desde);
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    Usuario
+        .find({}, 'nombre email') // Primer parámetro son las condiciones de búsqueda, segundo parámetro los campos que se van a devolver.
+        .skip(desde) // se salta los primeros N registros
+        .limit(limite) // muestra solo los primeros N registros de la búsqueda.
+        .exec((err, usuarios) => {
+            if (err)
+                return res.status(400).json({ ok: false, err });
+
+            res.json({ ok: true, usuarios });
+
+        });
+
+    // Para contar resultados.
+    Usuario.count({}, // Misma condición que en el find
+        (err, conteo) => {});
 });
 
 
@@ -32,14 +54,40 @@ app.post('/usuario', function(req, res) {
     );
 
 });
-// Recepción de parámetro por URL
+
+// Actualizar registro: requiere recepción de parámetro por URL
 app.put('/usuario/:id', function(req, res) {
     let id = req.params.id;
-    res.json({
-        msg: 'put a usuario',
-        id // No es necesario mapear 'id'
-    });
+
+    let body = _.pick(req.body, // El body está disponible gracias al paquete body-parser instalado.
+        ['nombre', 'email', 'img', 'role', 'estado']); // Listado de campos que sí se pueden actualizar.
+
+    // Alternativa a usar el pick de underscore: 
+    // quitamos explícitamente los campos que nos queremos actualizar.
+    // delete body.google; 
+    // delete body.password; 
+
+    // Doc: https://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate
+    Usuario.findByIdAndUpdate(id, body, // objeto que queremos actualizar, directamente el body
+        {
+            new: true, // enviar respuesta con el objeto actualizado en lugar del original.
+            runValidators: true
+        }, // aplicar validaciones definidas en el esquema (por ejemplo roles definidos)
+        (err, usuarioDB) => {
+
+            if (err)
+                return res.status(400).json({ ok: false, err });
+
+            res.json({
+                ok: true,
+                id, // No es necesario mapear 'id'
+                usuario: usuarioDB
+            });
+        });
+
+
 });
+
 app.delete('/usuario', function(req, res) {
     res.json('delete a usuario');
 });
